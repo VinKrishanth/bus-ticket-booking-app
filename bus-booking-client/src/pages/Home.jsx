57b7d7045c1122a765731ddd206bfcd86f06e516
vin <krishanth.cse@gmail.com>
2025-01-11 13:51:29 +0530

    Created new components  SectionCard, Features, PaymentOption, HeroSection

--- bus-booking-client/src/pages/Home.jsx
+++ bus-booking-client/src/pages/Home.jsx
@@ -1,24 +1,34 @@
-import SearchForm from '../components/SearchForm'
+import HeroSection from '../components/home/HeroSection'
+import SectionCard from '../components/home/SectionCard'
 
 export default function Home() {
   return (
-    <div className="min-h-[calc(100vh-4rem)] bg-gradient-to-br from-white via-white to-secondary">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="grid md:grid-cols-2 gap-12 items-center">
-          <div>
-            <h2 className="text-4xl md:text-5xl font-bold text-gray-800 leading-tight mb-8">
-              Get Your Ticket Online,<br />Easy and Safely
-            </h2>
-            <button className="bg-primary text-white px-8 py-3 rounded-md font-semibold hover:bg-primary/90 transition">
-              GET TICKET NOW
-            </button>
-          </div>
+    <>
+      <HeroSection />
+      {
+        [
+        {
+          Option:'Payment Options',
+          title:'Multiple Payment Options'
+        },
+        {
+          Option:'',
+          title:'Why Book with BusSeat?'
+        },
+        ].map((info, index) => {
           
-          <div>
-            <SearchForm />
-          </div>
-        </div>
-      </div>
-    </div>
+        })
+      }
+      <SectionCard 
+        Option='Payment Options'
+        title='Multiple Payment Options'
+        key={1}
+      />
+      <SectionCard 
+        Option=''
+        title='Why Book with BusSeat?'
+        key={2}
+      />
+    </>
   )
 }
--- bus-booking-client/src/utils/index.js
+++ bus-booking-client/src/utils/index.js
@@ -4,3 +4,13 @@ export { default as Dashboard } from '../pages/Dashboard';
 export { default as Login } from '../pages/Login';
 export { default as SearchBuses } from '../pages/SearchBuses';
 export { default as Navbar } from '../components/header/Navbar';
+
+export { default as AmericaCard } from '../assets/images/amexcard-96.png';
+export { default as Genie } from '../assets/images/genie.png';
+export { default as Map } from '../assets/images/mapmarker-96.png';
+export { default as Mastercard } from '../assets/images/mastercard-96.png';
+export { default as MoneyTransfer } from '../assets/images/moneytrasnfer-96.png';
+export { default as Search } from '../assets/images/search-96.png';
+export { default as Support } from '../assets/images/support-96.png';
+export { default as Ticket } from '../assets/images/ticket-96.png';
+export { default as VisaCard } from '../assets/images/visacard-96.png';
